c69a2bd7a0e08762422c6898646f0bfd48588f06
Wilson13 <wilsonxin@hotmail.com>
2015-08-26 21:47:45 +0800

    First commit

---
+++ fizzbuzz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	int i;
+	for(i=0;i<100;i++){
+		printf("The current 'i' is %d ", i);
+		if(i%3==0)
+			printf("Fizz");
+		if(i%5==0)
+			printf("Buzz");
+		printf("\n");
+				
+	}
+	return 0;
+}
